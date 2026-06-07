d0ba111fd0d5c9f0f514842fe97f9a77352b57b5
Kjur0 <82160866+Kjur0@users.noreply.github.com>
2026-06-07 05:26:57 +0000

    deps: update lolhtml to v3.0.0 (02f139c4437b2da666a50d32e11d9158cbe0a393)

--- scripts/build/deps/lolhtml.ts
+++ scripts/build/deps/lolhtml.ts
@@ -9,7 +9,7 @@
 import type { Config } from "../config.ts";
 import type { CargoBuild, Dependency } from "../source.ts";
 
-const LOLHTML_COMMIT = "77127cd2b8545998756e8d64e36ee2313c4bb312";
+const LOLHTML_COMMIT = "02f139c4437b2da666a50d32e11d9158cbe0a393";
 
 /**
  * -Zbuild-std requires an explicit --target even when host == target.
